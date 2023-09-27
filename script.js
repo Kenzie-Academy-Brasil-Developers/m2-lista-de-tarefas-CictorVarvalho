ab76dc8d93de3619a7319ce56ef994e3c7a89e85
Victor Carvalho de Jesus <cictorvarvalho@gmail.com>
2023-09-27 09:16:40 -0300

    att 2

--- script.js
+++ script.js
@@ -10,3 +10,47 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+
+function renderElements(arr) {
+  function createTaskItem(obj){
+  
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    const span = document.createElement("span");
+    const p = document.createElement("p");
+    const button = document.createElement("button");
+    
+    ul.appendChild(li);
+    li.classList.add("task__item");
+    
+    li.appendChild(div);
+    div.classList.add("task-info__container");
+    
+    div.appendChild(span);
+    span.classList.add("task-type");
+    span.textContent = "•";
+    if (obj.type === "Urgente") {
+      span.classList.add("span-urgent");
+    }
+    else if (obj.type === "Importante") {
+      span.classList.add("span-important");
+    } else if (obj.type === "Normal") {
+      span.classList.add("span-normal");
+    }
+    
+    div.appendChild(p);
+    p.textContent = obj.title;
+  
+    li.appendChild(button);
+    button.classList.add("task__button--remove-task");
+    
+  }
+  const ul = document.querySelector("ul");
+  
+  
+  for (let i = 0; i < arr.length; i++) {
+    createTaskItem(tasks[i]);
+  }
+}
+
+renderElements(tasks);
