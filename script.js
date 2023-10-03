6d8c4a9b96c797ba89db42b70adc74f9990fecdc
Victor Carvalho de Jesus <cictorvarvalho@gmail.com>
2023-10-03 13:46:04 -0300

    ultima att

--- script.js
+++ script.js
@@ -11,46 +11,76 @@ const tasks = [
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
 
-function renderElements(arr) {
-  function createTaskItem(obj){
-  
-    const li = document.createElement("li");
-    const div = document.createElement("div");
-    const span = document.createElement("span");
-    const p = document.createElement("p");
-    const button = document.createElement("button");
-    
-    ul.appendChild(li);
-    li.classList.add("task__item");
-    
-    li.appendChild(div);
-    div.classList.add("task-info__container");
-    
-    div.appendChild(span);
-    span.classList.add("task-type");
-    span.textContent = "•";
-    if (obj.type === "Urgente") {
-      span.classList.add("span-urgent");
-    }
-    else if (obj.type === "Importante") {
-      span.classList.add("span-important");
-    } else if (obj.type === "Normal") {
-      span.classList.add("span-normal");
-    }
-    
-    div.appendChild(p);
-    p.textContent = obj.title;
-  
-    li.appendChild(button);
-    button.classList.add("task__button--remove-task");
-    
-  }
-  const ul = document.querySelector("ul");
-  
-  
+function renderElements(arr) { 
   for (let i = 0; i < arr.length; i++) {
-    createTaskItem(tasks[i]);
+    createTaskItem(arr[i]);
   }
 }
 
 renderElements(tasks);
+
+function createTaskItem(obj){
+  const ul = document.querySelector("ul");
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  const span = document.createElement("span");
+  const p = document.createElement("p");
+  const button = document.createElement("button");
+  
+  ul.appendChild(li);
+  li.classList.add("task__item");
+  
+  li.append(div, button);
+  
+  div.classList.add("task-info__container"); 
+  div.append(span, p);
+  
+  p.textContent = obj.title;
+
+  span.classList.add("task-type");
+  span.textContent = "•";
+  if (obj.type === "Urgente") {
+    span.classList.add("span-urgent");
+  }
+  else if (obj.type === "Importante") {
+    span.classList.add("span-important");
+  } else if (obj.type === "Normal") {
+    span.classList.add("span-normal");
+  }
+
+  button.classList.add("task__button--remove-task");
+  button.addEventListener('click', function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    
+    let indexObj = tasks.indexOf(obj);
+    tasks.splice(indexObj, 1);
+
+    ul.innerHTML = "";
+    renderElements(tasks);
+  })
+  
+}
+
+let form = document.querySelector(".form__container");
+
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  event.stopPropagation();
+  const ul = document.querySelector("ul");
+  const inputTitle = document.querySelector(".form__input--text");
+  const inputType = document.querySelector(".form__input--priority");
+  if (inputTitle.value === "" || inputType.value === "") {
+    alert("Preencha o título ou selecione o tipo da tarefa");
+  } else {
+    const newTask = {
+      title: inputTitle.value,
+      type: inputType.value
+    }
+  
+    tasks.push(newTask);
+    ul.innerHTML = "";
+    renderElements(tasks);
+  }
+  
+})
